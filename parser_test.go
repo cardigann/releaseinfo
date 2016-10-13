42110b43821fdb38e55b4bcfb28fb6ac2ae0be7f
lxt2 <rznqsp8ffyeprn2l@gmail.com>
2016-10-13 13:48:08 +1100

    Add tests for multi-episodes

--- parser_test.go
+++ parser_test.go
@@ -768,7 +768,7 @@ func TestParsingPath(t *testing.T) {
 	}
 }
 
-func TestParsigAnimeMetadata(t *testing.T) {
+func TestParsingAnimeMetadata(t *testing.T) {
 	for idx, test := range []struct {
 		title          string
 		subgroup, hash string
@@ -797,5 +797,75 @@ func TestParsigAnimeMetadata(t *testing.T) {
 			fmt.Sprintf("Row %d should have correct release group", idx+1))
 		require.Equal(t, test.hash, result.ReleaseHash,
 			fmt.Sprintf("Row %d should have correct release hash", idx+1))
+	}
+}
+
+func TestParsingMultiEpisodeTitles(t *testing.T) {
+	for idx, test := range []struct {
+		title         string
+		expectedTitle string
+		season        int
+		episodes      []int
+	}{
+		{"WEEDS.S03E01-06.DUAL.BDRip.XviD.AC3.-HELLYWOOD", "WEEDS", 3, []int{1, 2, 3, 4, 5, 6}},
+		{"Two.and.a.Half.Men.103.104.720p.HDTV.X264-DIMENSION", "Two and a Half Men", 1, []int{3, 4}},
+		{"Weeds.S03E01.S03E02.720p.HDTV.X264-DIMENSION", "Weeds", 3, []int{1, 2}},
+		{"The Borgias S01e01 e02 ShoHD On Demand 1080i DD5 1 ALANiS", "The Borgias", 1, []int{1, 2}},
+		{"White.Collar.2x04.2x05.720p.BluRay-FUTV", "White Collar", 2, []int{4, 5}},
+		{"Desperate.Housewives.S07E22E23.720p.HDTV.X264-DIMENSION", "Desperate Housewives", 7, []int{22, 23}},
+		{"Desparate Housewives - S07E22 - S07E23 - And Lots of Security.. [HDTV-720p].mkv", "Desparate Housewives", 7, []int{22, 23}},
+		{"S03E01.S03E02.720p.HDTV.X264-DIMENSION", "", 3, []int{1, 2}},
+		{"Desparate Housewives - S07E22 - 7x23 - And Lots of Security.. [HDTV-720p].mkv", "Desparate Housewives", 7, []int{22, 23}},
+		{"S07E22 - 7x23 - And Lots of Security.. [HDTV-720p].mkv", "", 7, []int{22, 23}},
+		{"2x04x05.720p.BluRay-FUTV", "", 2, []int{4, 5}},
+		{"S02E04E05.720p.BluRay-FUTV", "", 2, []int{4, 5}},
+		{"S02E03-04-05.720p.BluRay-FUTV", "", 2, []int{3, 4, 5}},
+		{"Breakout.Kings.S02E09-E10.HDTV.x264-ASAP", "Breakout Kings", 2, []int{9, 10}},
+		{"Breakout Kings - 2x9-2x10 - Served Cold [SDTV] ", "Breakout Kings", 2, []int{9, 10}},
+		{"Breakout Kings - 2x09-2x10 - Served Cold [SDTV] ", "Breakout Kings", 2, []int{9, 10}},
+		{"Hell on Wheels S02E09 E10 HDTV x264 EVOLVE", "Hell on Wheels", 2, []int{9, 10}},
+		{"Hell.on.Wheels.S02E09-E10.720p.HDTV.x264-EVOLVE", "Hell on Wheels", 2, []int{9, 10}},
+		{"Grey's Anatomy - 8x01_02 - Free Falling", "Grey's Anatomy", 8, []int{1, 2}},
+		{"8x01_02 - Free Falling", "", 8, []int{1, 2}},
+		{"Kaamelott.S01E91-E100", "Kaamelott", 1, []int{91, 92, 93, 94, 95, 96, 97, 98, 99, 100}},
+		{"Neighbours.S29E161-E165.PDTV.x264-FQM", "Neighbours", 29, []int{161, 162, 163, 164, 165}},
+		{"Shortland.Street.S22E5363-E5366.HDTV.x264-FiHTV", "Shortland Street", 22, []int{5363, 5364, 5365, 5366}},
+		{"the.office.101.102.hdtv-lol", "the office", 1, []int{1, 2}},
+		{"extant.10708.hdtv-lol.mp4", "extant", 1, []int{7, 8}},
+		{"extant.10910.hdtv-lol.mp4", "extant", 1, []int{9, 10}},
+		{"E.010910.HDTVx264REPACKLOL.mp4", "E", 1, []int{9, 10}},
+		{"World Series of Poker - 2013x15 - 2013x16 - HD TV.mkv", "World Series of Poker", 2013, []int{15, 16}},
+		{"The Librarians US S01E01-E02 720p HDTV x264", "The Librarians US", 1, []int{1, 2}},
+		{"Series Title Season 01 Episode 05-06 720p", "Series Title", 1, []int{5, 6}},
+		{"The Young And The Restless - S42 Ep10718 - Ep10722", "The Young And The Restless", 42, []int{10718, 10719, 10720, 10721, 10722}},
+		{"The Young And The Restless - S42 Ep10688 - Ep10692", "The Young And The Restless", 42, []int{10688, 10689, 10690, 10691, 10692}},
+		{"RWBY.S01E02E03.1080p.BluRay.x264-DeBTViD", "RWBY", 1, []int{2, 3}},
+		{"grp-zoos01e11e12-1080p", "grp-zoo", 1, []int{11, 12}},
+		{"grp-zoo-s01e11e12-1080p", "grp-zoo", 1, []int{11, 12}},
+		{"Series Title.S6.E1.E2.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2}},
+		{"Series Title.S6E1-E2.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2}},
+		{"Series Title.S6E1-S6E2.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2}},
+		{"Series Title.S6E1E2.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2}},
+		{"Series Title.S6E1-E2-E3.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2, 3}},
+		{"Series Title.S6.E1E3.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2, 3}},
+		{"Series Title.S6.E1-E2.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2}},
+		{"Series Title.S6.E1-S6E2.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2}},
+		{"Series Title.S6.E1E2.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2}},
+		{"Series Title.S6.E1-E2-E3.Episode Name.1080p.WEB-DL", "Series Title", 6, []int{1, 2, 3}},
+	} {
+		result, err := ParseTitle(test.title)
+
+		require.NoError(t, err,
+			fmt.Sprintf("Row %d should have no error", idx+1))
+		require.Equal(t, test.expectedTitle, result.SeriesTitleInfo.Title,
+			fmt.Sprintf("Row %d should have correct title", idx+1))
+		require.Equal(t, test.season, result.SeasonNumber,
+			fmt.Sprintf("Row %d should have correct season", idx+1))
+		require.Equal(t, test.episodes, result.EpisodeNumbers,
+			fmt.Sprintf("Row %d should have correct episodes", idx+1))
+		require.Len(t, result.AbsoluteEpisodeNumbers, 0,
+			fmt.Sprintf("Row %d should have 0 absolute episode numbers", idx+1))
+		require.False(t, result.FullSeason,
+			fmt.Sprintf("Row %d should not be a full season)", idx+1))
 	}
 }
