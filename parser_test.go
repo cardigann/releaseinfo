78e5355f24472caf289794ebc00db1f687f497cd
lxt2 <rznqsp8ffyeprn2l@gmail.com>
2016-10-13 13:48:00 +1100

    Add path parsing tests

--- parser_test.go
+++ parser_test.go
@@ -723,3 +723,47 @@ func TestParsingReleaseGroup(t *testing.T) {
 			fmt.Sprintf("Row %d %q should have the correct release group", idx+1, test.postTitle))
 	}
 }
+
+func TestParsingPath(t *testing.T) {
+	for idx, test := range []struct {
+		path            string
+		season, episode int
+	}{
+		{`z:\tv shows\battlestar galactica (2003)\Season 3\S03E05 - Collaborators.mkv`, 3, 5},
+		{`z:\tv shows\modern marvels\Season 16\S16E03 - The Potato.mkv`, 16, 3},
+		{`z:\tv shows\robot chicken\Specials\S00E16 - Dear Consumer - SD TV.avi`, 0, 16},
+		{`D:\shares\TV Shows\Parks And Recreation\Season 2\S02E21 - 94 Meetings - 720p TV.mkv`, 2, 21},
+		{`D:\shares\TV Shows\Battlestar Galactica (2003)\Season 2\S02E21.avi`, 2, 21},
+		{`C:/Test/TV/Chuck.4x05.HDTV.XviD-LOL`, 4, 5},
+		{`P:\TV Shows\House\Season 6\S06E13 - 5 to 9 - 720p BluRay.mkv`, 6, 13},
+		{`S:\TV Drop\House - 10x11 - Title [SDTV]\1011 - Title.avi`, 10, 11},
+		{`/TV Drop/House - 10x11 - Title [SDTV]/1011 - Title.avi`, 10, 11},
+		{`S:\TV Drop\King of the Hill - 10x12 - 24 Hour Propane People [SDTV]\1012 - 24 Hour Propane People.avi`, 10, 12},
+		{`/TV Drop/King of the Hill - 10x12 - 24 Hour Propane People [SDTV]/1012 - 24 Hour Propane People.avi`, 10, 12},
+		{`S:\TV Drop\King of the Hill - 10x12 - 24 Hour Propane People [SDTV]\Hour Propane People.avi`, 10, 12},
+		{`/TV Drop/King of the Hill - 10x12 - 24 Hour Propane People [SDTV]/Hour Propane People.avi`, 10, 12},
+		{`E:\Downloads\tv\The.Big.Bang.Theory.S01E01.720p.HDTV\ajifajjjeaeaeqwer_eppj.avi`, 1, 1},
+		{`C:\Test\Unsorted\The.Big.Bang.Theory.S01E01.720p.HDTV\tbbt101.avi`, 1, 1},
+		{`C:\Test\Unsorted\Terminator.The.Sarah.Connor.Chronicles.S02E19.720p.BluRay.x264-SiNNERS-RP\ba27283b17c00d01193eacc02a8ba98eeb523a76.mkv`, 2, 19},
+		{`C:\Test\Unsorted\Terminator.The.Sarah.Connor.Chronicles.S02E18.720p.BluRay.x264-SiNNERS-RP\45a55debe3856da318cc35882ad07e43cd32fd15.mkv`, 2, 18},
+		{`C:\Test\Series\Season 01\01 Pilot (1080p HD).mkv`, 1, 1},
+		{`C:\Test\Series\Season 01\1 Pilot (1080p HD).mkv`, 1, 1},
+		{`C:\Test\Series\Season 1\02 Honor Thy Father (1080p HD).m4v`, 1, 2},
+		{`C:\Test\Series\Season 1\2 Honor Thy Father (1080p HD).m4v`, 1, 2},
+	} {
+		result, err := ParsePath(test.path)
+
+		require.NoError(t, err,
+			fmt.Sprintf("Row %d should have no error", idx+1))
+		require.Len(t, result.EpisodeNumbers, 1,
+			fmt.Sprintf("Row %d should have 1 episode", idx+1))
+		require.Equal(t, test.season, result.SeasonNumber,
+			fmt.Sprintf("Row %d should have correct season", idx+1))
+		require.Equal(t, test.episode, result.EpisodeNumbers[0],
+			fmt.Sprintf("Row %d should have correct episode", idx+1))
+		require.Len(t, result.AbsoluteEpisodeNumbers, 0,
+			fmt.Sprintf("Row %d should have no absolute episodes", idx+1))
+		require.False(t, result.FullSeason,
+			fmt.Sprintf("Row %d should not be a full season", idx+1))
+	}
+}
